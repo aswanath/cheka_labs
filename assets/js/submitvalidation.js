d22c404c8d8a23ad5899423264230943ffb87b2a
Aswanath Chelorakkandy <aswanathck.ramesh@gmail.com>
2022-01-22 13:20:32 +0530

    first commit

--- assets/js/submitvalidation.js
+++ assets/js/submitvalidation.js
@@ -1,7 +1,12 @@
 $("#message, #name, #email, #subject").on("keyup", function(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if($("#message").val().trim().length >=2  && $("#name").val().trim().length >=2
-     && $("#email").val().trim().length >=6 && $("#email").val().indexOf('@') > -1 && $("#email").val().indexOf('.') > -1
+     && $("#email").val().match(mailformat)
   && $("#subject").val().trim().length >=2){
         $("#submitbutton").removeAttr("disabled");
+        $("#submitbutton").removeClass('no-hover');
+    }else{
+        $("#submitbutton").attr('disabled','disabled');
+        $("#submitbutton").addClass('no-hover');
     }
 });
