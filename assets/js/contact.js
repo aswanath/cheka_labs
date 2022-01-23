8d49842649479a0496888306d697e397658b35f8
Aswanath Chelorakkandy <aswanathck.ramesh@gmail.com>
2022-01-23 11:00:36 +0530

    first commit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -12,17 +12,9 @@ $(document).ready(function(){
     $(function() {
         $('#contactForm').validate({
             rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
                 subject: {
                     required: true,
                     minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
                 },
                 email: {
                     required: true,
@@ -34,20 +26,13 @@ $(document).ready(function(){
                 }
             },
             messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
                 subject: {
                     required: "come on, you have a subject, don't you?",
                     minlength: "your subject must consist of at least 4 characters"
                 },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
                 email: {
-                    required: "no email, no message"
+                    required: "no email, no message",
+                    email : "please enter a valid email"
                 },
                 message: {
                     required: "um...yea, you have to write something to send this form.",
